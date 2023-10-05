0aee67c2bea74443fb137c3b69e8af6f6ff300b1
FrancescoFucile <francesc.fucile@ocyan.com>
2023-10-06 00:10:22 +0100

    refactor and restructure

--- request.js
+++ request.js
@@ -4,26 +4,26 @@ const notion = new Client({ auth: process.env.NOTION_API_KEY });
 
 async function build() {
 	try {
-		// retrieve menu list
+		// retrieve menu list.
 		const databaseId = 'dba2b39a6cb74962a01c998c9c78e229';
 		results = {}
 		results.menus = await notion.databases.query({ database_id: databaseId, });
-		// list first menu blocks
+		// list first menu blocks.
 		results.first_menu_blocks = await notion.blocks.children.list({ block_id: results.menus.results[0].id });
-		// read the menu database
+		// read the day-meal-recipes database.
 		results.first_menu_db = await notion.databases.query({ database_id: results.first_menu_blocks.results[0].id });
-		// extract meals for the first day dinner
-		results.first_day_dinner_first_meal = await notion.blocks.retrieve({ block_id: results.first_menu_db.results[0].properties.Cena.relation[0].id })
-		// map meals for the day meals
-		results.meals_map = await Promise.all(results.first_menu_db.results.map(async day => day.properties.Name.title[0]?.plain_text)
-			.map(async day => {
+		// build day-pranzo/cena map.
+		results['day-pranzo/cena'] = await Promise.all(reduceObject(results.first_menu_db, ['results'])
+			.results.map(result => reduceObject(result, 'properties'))
+			.map(async entry => {
 				return {
-					[await day]: {
-						Pranzo: await getCourses("Pranzo"),
-						Cena: await getCourses("Cena")
-					}
+					day: entry.properties.Name.title[0].plain_text,
+					pranzo:  await Promise.all(entry.properties.Pranzo.relation.map( async relation =>
+						await notion.blocks.retrieve({ block_id: relation.id }).then((result) => result.child_page.title)
+					)),
 				}
 			}))
+
 	} catch (e) {
 		console.log("Failed", e)
 	} finally {
@@ -31,10 +31,20 @@ async function build() {
 	}
 }
 
-// given the name of a meal, retrieve its courses
+// given the name of a meal, retrieve its courses.
 async function getCourses(meal) {
 	return await Promise.all(results.first_menu_db.results[0].properties[meal].relation.map(
 		async relation => await notion.blocks.retrieve({ block_id: relation.id }).then((result) => result.child_page.title)))
+}
+
+// filter object for keys.
+function reduceObject(object, allowedFields) {
+	return Object.keys(object).reduce(function (newObj, key) {
+		if (allowedFields.indexOf(key) !== -1) {
+			newObj[key] = object[key];
+		}
+		return newObj;
+	}, {});
 }
 
 build().then(console.log(''))
