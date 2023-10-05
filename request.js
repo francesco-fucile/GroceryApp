d7b1c3e821b4fea75cc700b800f4fee17fee5012
FrancescoFucile <francesc.fucile@ocyan.com>
2023-10-05 18:55:40 +0100

    map meals for the first day dinner

--- request.js
+++ request.js
@@ -2,27 +2,32 @@ const { Client } = require('@notionhq/client');
 const { error } = require('console');
 const notion = new Client({ auth: process.env.NOTION_API_KEY });
 
-(async () => {
+async function build() {
 	try {
 		// retrieve menu list
 		const databaseId = 'dba2b39a6cb74962a01c998c9c78e229';
 		results = {}
 		results.menus = await notion.databases.query({ database_id: databaseId, });
 		// list first menu blocks
-		results.first_menu_blocks = await notion.blocks.children.list({ block_id: results.menus.results[0].id, });
+		results.first_menu_blocks = await notion.blocks.children.list({ block_id: results.menus.results[0].id });
 		// read the menu database
-		results.first_menu_db = await notion.databases.query({ database_id: results.first_menu_blocks.results[0].id, });
+		results.first_menu_db = await notion.databases.query({ database_id: results.first_menu_blocks.results[0].id });
 		// extract meals for the first day dinner
 		results.first_day_dinner_first_meal = await notion.blocks.retrieve({ block_id: results.first_menu_db.results[0].properties.Cena.relation[0].id })
-		// build day meal for first meal of first day dinner
-		results.first_day_dinner_first_meal_map = { [results.first_menu_db.results[5].properties.Name.title[0].text.content]: results.first_day_dinner_first_meal.child_page.title }
+		// map meals for the first day dinner
+		results.first_day_dinner_first_meal_map = {
+			[results.first_menu_db.results[5].properties.Name.title[0].text.content]:
+				await Promise.all(results.first_menu_db.results[0].properties.Cena.relation.map(
+					async relation => await notion.blocks.retrieve({ block_id: relation.id }).then( (result) => result.child_page.title )
+				))
+		}
 	} catch (e) {
 		console.log("Failed", e)
 	} finally {
 		console.log(JSON.stringify(results, null, 2))
 	}
-})();
+}
 
-
+build().then(console.log(''))
 
 
