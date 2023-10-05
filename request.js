ad03e1687bcc2f4be8cfda0bbee8b7b1a033fbe2
FrancescoFucile <francesc.fucile@ocyan.com>
2023-10-05 18:27:10 +0100

    build day meal for first meal of first day dinner

---
+++ request.js
@@ -0,0 +1,28 @@
+const { Client } = require('@notionhq/client');
+const { error } = require('console');
+const notion = new Client({ auth: process.env.NOTION_API_KEY });
+
+(async () => {
+	try {
+		// retrieve menu list
+		const databaseId = 'dba2b39a6cb74962a01c998c9c78e229';
+		results = {}
+		results.menus = await notion.databases.query({ database_id: databaseId, });
+		// list first menu blocks
+		results.first_menu_blocks = await notion.blocks.children.list({ block_id: results.menus.results[0].id, });
+		// read the menu database
+		results.first_menu_db = await notion.databases.query({ database_id: results.first_menu_blocks.results[0].id, });
+		// extract meals for the first day dinner
+		results.first_day_dinner_first_meal = await notion.blocks.retrieve({ block_id: results.first_menu_db.results[0].properties.Cena.relation[0].id })
+		// build day meal for first meal of first day dinner
+		results.first_day_dinner_first_meal_map = { [results.first_menu_db.results[5].properties.Name.title[0].text.content]: results.first_day_dinner_first_meal.child_page.title }
+	} catch (e) {
+		console.log("Failed", e)
+	} finally {
+		console.log(JSON.stringify(results, null, 2))
+	}
+})();
+
+
+
+
